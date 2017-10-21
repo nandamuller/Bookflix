4f9f31af330e1a61f0d64681aae6790482e1d764
nandamuller <mullermariafernanda@gmail.com>
2017-10-21 15:14:15 -0200

    Página de Cadastro
    Modificação na página de cadastro para deixar no mesmo estilo do Login.

--- web-ui/src/components/Cadastre.vue
+++ web-ui/src/components/Cadastre.vue
@@ -3,27 +3,27 @@
         <md-card class="bkf-conheca">
             <md-card-header>
                 <md-card-header-text>
-                    <div class="md-title">Cadastre</div>
+                    <div class="md-title">Cadastre-se</div>
                 </md-card-header-text>
             </md-card-header>
             <md-card-content style="min-height: 70vh;">
-                <form novalidate @submit.stop.prevent="submit">
-                                <md-input-container>
-                                <label>Nome</label>
-                                <md-input v-model="initialValue"></md-input>
-                                </md-input-container>
-
-                                <md-input-container>
-                                    <label>E-mail</label>
-                                    <md-input v-model="initialValue"></md-input>
-                                </md-input-container>
-
-                                <md-input-container>
-                                    <label>Senha</label>
-                                    <md-input type="password"></md-input>
-                                </md-input-container>
-                            </form> 
-                <md-button class="md-raised md-primary">Confirma</md-button>
+               <form>
+                    <div class="group">
+                        <input type="text"><span class="highlight"></span><span class="bar"></span>
+                        <label>Name</label>
+                    </div>
+                        <div class="group">
+                            <input type="email"><span class="highlight"></span><span class="bar"></span>
+                            <label>Email</label>
+                        </div>
+                        <div class="group">
+                            <input type="password"><span class="highlight"></span><span class="bar"></span>
+                            <label>Senha</label>
+                        </div>
+                          <button type="button" class="button buttonBlue">Cadastre-se
+                            <div class="ripples buttonRipples"><span class="ripplesCircle"></span></div>
+                          </button>
+                        </form>
             </md-card-content>
         </md-card>
     </md-whiteframe>
