cd35e2afd4b1b85fc968d615dfa7097020d55193
nandamuller <mullermariafernanda@gmail.com>
2017-10-23 16:04:35 -0200

    Alterações na página Configurações
    Adicionado o conteúdo da página sem CSS

--- web-ui/src/components/Configuracoes.vue
+++ web-ui/src/components/Configuracoes.vue
@@ -3,15 +3,23 @@
         <md-card class="bkf-conheca" style="min-height: 70vh;">
             <md-card-header>
                 <md-card-header-text>
-                    <div class="md-title">Configurações</div>
+                    <div class="md-title"><h1>Configurações</h1></div>
                 </md-card-header-text>
             </md-card-header>
 
-            <md-card-content>
+            <md-card-content class="bkf-conf">
                 <div>
                     <h1>
-                        
+                       Sua conta 
                     </h1>
+                <div>
+                    <p>Altere seu e-mail:</p>
+                    <input type="email"><span class="highlight"></span><span class="bar"></span>
+                </div>
+                <div>
+                    <p>Altere sua senha:</p>
+                    <input type="password"><span class="highlight"></span><span class="bar"></span>
+                </div>
                 </div>
             </md-card-content>
         </md-card>
@@ -33,5 +41,8 @@
      .bkf-conheca{
         margin:150px;
         margin-top: 0px;
+    }
+    .bkf-conf{
+        
     }
 </style>
