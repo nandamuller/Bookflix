66afe21a5dc66fa0e368da96e9ab197d797feb47
nandamuller <mullermariafernanda@gmail.com>
2017-10-24 11:27:09 -0200

    Página Biblioteca
    Começo da página biblioteca

--- web-ui/src/components/Biblioteca.vue
+++ web-ui/src/components/Biblioteca.vue
@@ -1,11 +1,32 @@
 <template>
-     <div>Em construção.</div>
+    <md-whiteframe>
+        <md-card class="bkf-conheca" style="min-height: 70vh;">
+            <md-card-header>
+                <md-card-header-text>
+                    <div><md-chip md-deletable></md-chip>
+                        <md-chip md-deletable></md-chip>
+                        <md-chip md-deletable></md-chip></div>
+                </md-card-header-text>
+            </md-card-header>
+
+            <md-card-content>
+                
+            </md-card-content>
+        </md-card>
+    </md-whiteframe>
 </template>
 
 <script>
-
 </script>
 
 <style>
+    .bkf-conheca{
+        margin:150px;
+        margin-top: 0px;
+    }
+    .txt-bkf.conheca{
+        font-size: 20px;
+        line-height: 40px;
+    }
     
 </style>
