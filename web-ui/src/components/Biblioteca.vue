9281dda4fd9c47f9361c9780c8f0e5c438b9eeb0
nandamuller <mullermariafernanda@gmail.com>
2017-10-24 15:10:59 -0200

    Slider de Trechos
    Foi adicionado o slider de trechos e o cabeçalho da página

--- web-ui/src/components/Biblioteca.vue
+++ web-ui/src/components/Biblioteca.vue
@@ -11,10 +11,27 @@
                         </md-bottom-bar >
                 </md-card-header-text>
             </md-card-header>
+                <div class="bkf-header">
+                    <h1>Conheça nossa Biblioteca</h1>
+                </div>
+                <div class="bkf-biblioteca" style="min-height: 70vh;">  
+                <md-boards :md-auto="true" :md-infinite="true" :md-duration="5000" :md-swipeable="true">
+                  <md-board id="slide1">
+                    <p>"Ao vencido, ódio ou compaixão;<br>
+                        ao vencedor, as batatas."</p>
+                  </md-board>
 
-            <md-card-content>
-                
-            </md-card-content>
+                  <md-board id="slide2">
+                    <p>"Ao vencido, ódio ou compaixão;<br>
+                              ao vencedor, as batatas."</p>
+                  </md-board>
+
+                  <md-board id="slide3">
+                    <p>"Ao vencido, ódio ou compaixão;<br>
+                              ao vencedor, as batatas."</p>
+                  </md-board>
+                </md-boards>
+            </div>
         </md-card>
     </md-whiteframe>
 </template>
@@ -30,6 +47,12 @@
     .txt-bkf.conheca{
         font-size: 20px;
         line-height: 40px;
+    }
+    .bkf-biblioteca{
+        margin-top: 100px;
+    }
+    .bkf-header{
+        margin-top: 75px;
     }
     
 </style>
