806de5bf763369491c54f21e9edb8b0794743f15
nandamuller <mullermariafernanda@gmail.com>
2017-10-26 16:49:44 -0200

    Criado projeto de servidor Web.

---
+++ web-server/src/main/java/bookflix/BookflixApplication.java
@@ -0,0 +1,12 @@
+package bookflix;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BookflixApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BookflixApplication.class, args);
+	}
+}
---
+++ web-server/src/test/java/bookflix/BookflixApplicationTests.java
@@ -0,0 +1,16 @@
+package bookflix;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class BookflixApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
