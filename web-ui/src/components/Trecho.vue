dde1b67280c407b0bad3ee690faffd283c1c90c2
nandamuller <mullermariafernanda@gmail.com>
2017-11-01 20:43:01 -0200

    Trecho carregado dinamicamente
    É realizada a comunicação com o repositório de Trechos através da API REST desenvolvida com o Spring Boot.

--- web-ui/src/components/Trecho.vue
+++ web-ui/src/components/Trecho.vue
@@ -9,8 +9,8 @@
                         </md-card-header>
 
                           <md-card-content style="min-height: 70vh;">
-                            "Ao vencido, ódio ou compaixão;<br>
-                              ao vencedor, as batatas."
+                            <div class="sucesso" v-show="!erro">"{{ trecho.conteudo }}"</div>
+                              <div class="falha" v-show="erro">Houve um erro. Por favor tente novamente mais tarde.</div>
                           </md-card-content>
                     </md-card>
                     <div class="trechobar-bkf" md-gutter>
@@ -24,7 +24,7 @@
                             <md-dialog-content><p>
                                             Quincas Borba é um romance escrito por Machado de Assis, desenvolvido em princípio como folhetim na revista A Estação, entre os anos de 1886 e 1891 para, em 1892, ser publicado definitivamente pela Livraria Garnier.<br>
                                             Data da primeira publicação: 1891<br>
-                                            Autor: Machado de Assis<br>
+                                            Autor: {{ trecho.autor }}<br>
                                             Idioma original: Língua portuguesa<br>
                                             Gênero: Ficção<br>
                                             Editora: Livraria Garnier (primeira edição)<br>
@@ -47,6 +47,7 @@
      
      data() {
          return {
+             erro: true,
              trecho: {}
          }
      },
@@ -62,7 +63,19 @@
   methods: {
       buscarDados() {
           console.log(this.$route.params);
-          //debugger;
+          // TODO usar this.$route.params.id para que o trecho carregue dinamicamente
+          // duvida: quando o trecho for apenas um componente e id do trecho for recebido 
+          // atraves de parametro e nao pela rota, como faz?
+          axios.get('http://localhost:9000/trechos/1')
+            .then((resp) => {
+              this.erro = false;
+              this.trecho = resp.data
+              console.log(resp)
+            })
+            .catch((err) => {
+              this.erro = true;
+              console.log(err)
+            });
       },
     openDialog(ref) {
       this.$refs[ref].open();
