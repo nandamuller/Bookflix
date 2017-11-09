5747e6dbff461ed733de3bcb72da68f4567d6538
nandamuller <mullermariafernanda@gmail.com>
2017-11-09 12:28:07 -0200

    Mudança no layout da página Trecho do Dia
    Padronização do layout

--- web-ui/src/components/TrechoDoDia.vue
+++ web-ui/src/components/TrechoDoDia.vue
@@ -1,12 +1,14 @@
 <template>
     <md-whiteframe>
+        <md-layout style="margin: 20px;">
         <md-card class="trecho-bkf">
             <div class="md-title"><h1>Trecho do Dia</h1></div>
-            <md-layout>
+            <md-layout class=trecho-do-dia>
                 <bkf-trecho :trecho="trecho" v-if="trecho != null"></bkf-trecho>
                 <div class="falha" v-else>Houve um erro. Por favor tente novamente mais tarde.</div>
             </md-layout>
         </md-card>
+        </md-layout>
     </md-whiteframe>
 </template>
 
@@ -50,9 +52,13 @@
 </script>
 
 <style>
+    .trecho-do-dia{
+        margin: 20px; 
+        min-height: 70vh;"
+    }
      .trecho-bkf{
         text-align: center; 
         width: 100%;
-        font-size: 24px; 
+        font-size: 24px;
     }
 </style>
