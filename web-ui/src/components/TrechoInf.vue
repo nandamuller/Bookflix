6d35156474393bc626d3aeda2e91586b9a12dafb
nandamuller <mullermariafernanda@gmail.com>
2017-11-19 15:40:07 -0200

    Criada a estrutura básica da página TrechoInf

--- web-ui/src/components/TrechoInf.vue
+++ web-ui/src/components/TrechoInf.vue
@@ -1,5 +1,21 @@
 <template>
-    
+    <div class="trecho-bkf">
+        <md-card md-elevation="0">
+              <md-card-content>
+                  <div>"{{ trecho.conteudo }}"<br>
+                      {{ trecho.autor }} em {{ trecho.obra }}
+                </div>
+              </md-card-content>
+                <div class="trechobar-bkf" md-gutter>
+                    <md-bottom-bar>
+                        <router-link to="/trecho-info">
+                            <md-bottom-bar-item md-icon="info outline" router-link to="/trecho-info">Info</md-bottom-bar-item>
+                        </router-link>
+                        <md-bottom-bar-item md-icon="near_me">Compartilhar</md-bottom-bar-item>
+                    </md-bottom-bar>                    
+                </div>
+            </md-card>
+        </div>
 </template>
 
 <script>
@@ -14,5 +30,5 @@
 </script>
 
 <style>
-    
+
 </style>
