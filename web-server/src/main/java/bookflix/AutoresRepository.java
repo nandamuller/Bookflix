db4cc4010134b79eb0f728797b650a4b8899b4c7
nandamuller <mullermariafernanda@gmail.com>
2017-11-23 00:54:19 -0200

    Adicionada a capacidade de pesquisar autores e trechos
    Para pesquisar autor fornecer parte do nome.
    Para pesquisar conteúdo fornecer parte do conteúdo.

--- web-server/src/main/java/bookflix/AutoresRepository.java
+++ web-server/src/main/java/bookflix/AutoresRepository.java
@@ -1,11 +1,15 @@
 package bookflix;
 
+import java.util.List;
+
 import org.springframework.data.repository.PagingAndSortingRepository;
+import org.springframework.data.repository.query.Param;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 import org.springframework.web.bind.annotation.CrossOrigin;
 
 @CrossOrigin(origins = {"http://localhost", "http://localhost:8090"})
 @RepositoryRestResource(collectionResourceRel = "autores", path = "autores")
 public interface AutoresRepository extends PagingAndSortingRepository<Autor, Long> {
-
+	
+	List<Autor> findByNomeContainingIgnoreCase(@Param("nome") String nome);
 }
