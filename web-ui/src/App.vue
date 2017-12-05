d98c9eb638d6bff3fe48ba7480fa47e97d1c496f
nandamuller <mullermariafernanda@gmail.com>
2017-12-05 10:31:32 -0200

    Mudança na fonte

--- web-ui/src/App.vue
+++ web-ui/src/App.vue
@@ -48,7 +48,7 @@ export default {
 
 <style>
 #app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  font-family: 'Montserrat', sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
