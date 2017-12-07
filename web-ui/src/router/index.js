39eb9eff04670aa54bc1b4222ae6672d631d03b3
JavAlisson <javalisson@gmail.com>
2017-12-07 15:39:12 -0200

    Componente "home" assume a página inicial e modificado o roteador para modo history.

--- web-ui/src/router/index.js
+++ web-ui/src/router/index.js
@@ -1,6 +1,5 @@
 import Vue from 'vue'
 import Router from 'vue-router'
-import Hello from '@/components/Hello'
 import Home from '@/components/Home'
 import Cadastre from '@/components/Cadastre'
 import Conheca from '@/components/Conheca'
@@ -16,13 +15,10 @@ import Trechos from '@/components/Trechos'
 Vue.use(Router)
 
 export default new Router({
+  mode: 'history',
   routes: [
     {
       path: '/',
-      name: 'Hello',
-      component: Hello
-    }, {
-      path: '/home',
       name: 'Home',
       component: Home
     }, {
